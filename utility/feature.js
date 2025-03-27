7e21142779c38e8291a1c07ff66405d5959c609b
Diya-Mukherjee2003 <diyamukherjee904@gmail.com>
2025-03-27 21:07:21 +0530

    error resolved almost

--- utility/feature.js
+++ utility/feature.js
@@ -11,8 +11,10 @@ export const generateCookie = (user,res,statusCode=200,message)=>{
     .cookie("token", token, {
          httpOnly: true,
          secure: true,   // ✅ Required for SameSite=None (Must be on HTTPS)
-         sameSite: "None",  // ✅ Allows cross-origin cookies
-         maxAge: 10 * 60 * 1000  // 10 minutes
+         sameSite: "None",
+         path: "/",           // Ensure it's accessible on all routes
+         domain: "https://blogging-website-mern-e4ai.onrender.com",  // ✅ Allows cross-origin cookies
+         maxAge: 24* 10 * 60 * 1000  // 10 minutes
     })
     .json({
          success: true,
