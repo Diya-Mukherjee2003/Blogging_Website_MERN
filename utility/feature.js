9386e9a27c079a762b66835809d609e228439731
Diya-Mukherjee2003 <diyamukherjee904@gmail.com>
2025-03-27 13:13:17 +0530

    God is with me

--- utility/feature.js
+++ utility/feature.js
@@ -9,8 +9,8 @@ export const generateCookie = (user,res,statusCode=200,message)=>{
     res.status(201).cookie("token",token,{
         httpOnly:true,
         maxAge:10*60*1000,
-        sameSite:process.env.NODE_ENV === "Develpoment" ? "lax":"none",
-        secure:process.env.NODE_ENV === "Develpoment"?false:true
+        sameSite:process.env.NODE_ENV === "Development" ? "lax":"none",
+        secure:process.env.NODE_ENV === "Development"?false:true
     }).json({
         success:true,
         message
